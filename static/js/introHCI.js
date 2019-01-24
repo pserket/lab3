19dd40e67c77589aa051f139f91e71ccddaf9203
Pete Sheurpukdi <psheurpu@ucsd.edu>
2019-01-24 10:12:52 -0800

    Lab3 connect to heroku

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$('#testjs').text("Please wait...")
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
+	$("a.thumbnail").click(projectClick);
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	//$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	$(".project-description").fadeToggle()
+
+    }
 }
